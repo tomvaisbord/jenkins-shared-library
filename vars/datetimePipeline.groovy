3a0100ccdad4ff32f407d8a274ccab4da5b3a4f5
Vaisbord Tom <vaisbordt@gmail.com>
2024-10-22 14:24:00 +0300

    update

---
+++ vars/datetimePipeline.groovy
@@ -0,0 +1,48 @@
+// vars/datetimePipeline.groovy
+def call(Map config) {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    checkout scm
+                }
+            }
+
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        dockerImage = docker.build("${config.dockerRepo}:${config.imageTag}")
+                    }
+                }
+            }
+
+            stage('Test Docker Image') {
+                steps {
+                    script {
+                        dockerImage.inside {
+                            sh 'curl -f http://localhost:5000 || exit 1'
+                        }
+                    }
+                }
+            }
+
+            stage('Push to DockerHub') {
+                steps {
+                    script {
+                        docker.withRegistry('https://index.docker.io/v1/', 'dockerhub-credentials') {
+                            dockerImage.push()
+                        }
+                    }
+                }
+            }
+
+            stage('Deploy Docker Image') {
+                steps {
+                    sh "docker run -d -p 5000:5000 ${config.dockerRepo}:${config.imageTag}"
+                }
+            }
+        }
+    }
+}
