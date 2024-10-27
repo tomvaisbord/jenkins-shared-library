bfaf1bd234795be1d4c1ee1784d38570c3b00bcc
Vaisbord Tom <vaisbordt@gmail.com>
2024-10-27 13:07:04 +0200

    update

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -4,8 +4,8 @@ def call(String imageName, String dockerCredentialsId, String buildNumber) {
     withCredentials([usernamePassword(credentialsId: dockerCredentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
         sh """
             docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-            docker tag ${imageName} ${DOCKER_USERNAME}/${imageName}:${buildNumber}
-            docker tag ${imageName} ${DOCKER_USERNAME}/${imageName}:latest
+            docker tag ${imageName}:${buildNumber} ${DOCKER_USERNAME}/${imageName}:${buildNumber}
+            docker tag ${imageName}:${buildNumber} ${DOCKER_USERNAME}/${imageName}:latest
             docker push ${DOCKER_USERNAME}/${imageName}:${buildNumber}
             docker push ${DOCKER_USERNAME}/${imageName}:latest
             docker logout
