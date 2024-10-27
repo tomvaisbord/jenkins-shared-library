25ccf2370a464f3e8285275c8e0ea3074fc7ae01
Vaisbord Tom <vaisbordt@gmail.com>
2024-10-27 13:04:36 +0200

    update

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -1,11 +1,13 @@
 // vars/pushDocker.groovy
-def call(String imageName, String dockerCredentialsId) {
-    echo "Pushing Docker Image ${imageName} to DockerHub..."
+def call(String imageName, String dockerCredentialsId, String buildNumber) {
+    echo "Pushing Docker Image ${imageName} to DockerHub with tags ${buildNumber} and latest..."
     withCredentials([usernamePassword(credentialsId: dockerCredentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
         sh """
             docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-            docker tag ${imageName} ${DOCKER_USERNAME}/${imageName.split('/')[-1]}:latest  # Use only the image name, not full path
-            docker push ${DOCKER_USERNAME}/${imageName.split('/')[-1]}:latest
+            docker tag ${imageName} ${DOCKER_USERNAME}/${imageName}:${buildNumber}
+            docker tag ${imageName} ${DOCKER_USERNAME}/${imageName}:latest
+            docker push ${DOCKER_USERNAME}/${imageName}:${buildNumber}
+            docker push ${DOCKER_USERNAME}/${imageName}:latest
             docker logout
         """
     }
